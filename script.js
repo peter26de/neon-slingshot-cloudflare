f07a33a5daf6987bf42163b35ff5ae3178824930
peter26de <75163295+peter26de@users.noreply.github.com>
2026-07-07 21:24:47 +0200

    Update script.js

--- script.js
+++ script.js
@@ -564,7 +564,9 @@ function animate() {
 		
 					player.oldX = player.x;
 					player.oldY = player.y;
-					
+
+					player.vx *= 2 * Math.random() - 1;
+					player.vy *= 2 * Math.random() - 1;
 					if(Math.random() < 0.05) {
 						player.vx /= 20;
 						player.vy /= 20;
